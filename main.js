6e098e3f266fa39ebe937f4c0ba31052f28a7cc8
demo <demo@gmail.com>
2019-12-30 17:37:35 +0530

    Version 1.1.5 update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow, ipcMain } = require('electron')
-const { autoUpdater } = require('electron-updater');
+const { autoUpdater } = require('update-electron-app')();
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
