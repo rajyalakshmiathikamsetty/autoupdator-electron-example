9b9c0ce150696d6029599a54598a8a5588de85c9
demo <demo@gmail.com>
2020-01-13 15:52:52 +0530

    Autoupdater changes

--- main.js
+++ main.js
@@ -39,10 +39,14 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
-  
   console.log("testing the app");
   createWindow();
+  if (this.isDev) {
+     autoUpdater.checkForUpdates();
+   } else {
   autoUpdater.checkForUpdatesAndNotify();
+  }
+  //autoUpdater.checkForUpdatesAndNotify();
 })
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -58,7 +62,6 @@ app.on('window-all-closed', function () {
 // });
 
 ipcMain.on('app_version', (event) => {
-  console.log("event called"+app);
   event.sender.send('app_version', { version: app.getVersion() });
 });
 
@@ -68,16 +71,9 @@ autoUpdater.on('update-available', () => {
   
 })
 
-autoUpdater.on('download-progress', (progressObj) => {
-  let log_message = "Download speed: " + progressObj.bytesPerSecond;
-  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-  sendStatusToWindow(log_message, appversion);
-})
-
 autoUpdater.on('update-downloaded', (info) => {
-  setTimeout(function () {
       mainWindow.webContents.send('update_downloaded');
+      setTimeout(function () {
       autoUpdater.quitAndInstall();
   }, 5000)
 });
@@ -107,19 +103,4 @@ autoUpdater.on('error', (err) => {
   console.log('Error in auto-updater. ' + err);
 })
 
-// setupDevelopmentEnvironment() {
-//   mainWindow.openDevTools();
-//   mainWindow.webContents.on('context-menu', (e, props) => {
-//     const { x, y } = props;
-//     Menu
-//       .buildFromTemplate([{
-//         label: 'Inspect element',
-//         click: () => {
-//           mainWindow.inspectElement(x, y);
-//         }
-//       }])
-//       .popup(mainWindow);
-//   });
-// };
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+
